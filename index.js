3b6239a32aca2d87f672b4c4c4ce6474dbd1a4d7
James Halliday <mail@substack.net>
2013-06-23 05:09:43 -0700

    date escape codes, env expansion

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var path = require('path');
+var strftime = require('strftime');
 var re = RegExp(
     '\\\\[adehHjlnrstT@AuvVwW!#$\\\\[\\]]|\\\\[0-7]{3}'
-        + '|\\${[^}]*}',
+        + '|(\\$|\\\\D){[^}]*}',
     'g'
 );
 
@@ -13,15 +14,23 @@ module.exports = function (str, opts) {
     
     function replacer (s) {
         var m;
-        if (m = /^\${/.exec(s)) {
+        if (m = /^\${([^}]+)}/.exec(s)) {
             return opts.env[m[1]] || '';
         }
+        else if (m = /^\$(\w+)/.exec(s)) {
+            return opts.env[m[1]];
+        }
+        
         var x = s.slice(1);
         if (/^[0-7]{1,3}$/.test(x)) {
             return String.fromCharCode(parseInt(x, 8));
         }
         if (x === 'w') return process.cwd();
         if (x === 'W') return path.basename(process.cwd());
+        if (x === 'd') return strftime('%a %b %d', opts.now);
+        if (m = /^D{([^}]*)}/.exec(x)) {
+            return strftime(m[1], opts.now);
+        }
         
         var rep = {
             a: '\x07',
