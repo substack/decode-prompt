2f0f00596b657860cca6c62a83ba8d1bf3763630
James Halliday <mail@substack.net>
2013-06-23 23:24:04 -0700

    fix browser tests

--- test/os.js
+++ test/os.js
@@ -10,7 +10,7 @@ test('os codes', function (t) {
     t.equal(decode('\\l'), '-1');
     t.equal(decode('\\s', { shell: '/bin/bash' }), 'bash');
     t.equal(decode('\\u', { user: 'xyz' }), 'xyz');
-    t.equal(decode('\\u', { env: process.env }), process.env.USER);
+    t.equal(decode('\\u', { env: { USER: 'xxx' } }), 'xxx');
     t.equal(decode('\\v', { version: '1.2.3' }), '1.2');
     t.equal(decode('\\V', { version: '1.2.3' }), '1.2.3');
     t.equal(decode('\\!'), '0');
