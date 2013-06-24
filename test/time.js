7aa8acbbe25d53e081825359bf3a6e50281d18ed
James Halliday <mail@substack.net>
2013-06-23 22:10:57 -0700

    split time tests into time.js

---
+++ test/time.js
@@ -0,0 +1,24 @@
+var test = require('tape');
+var decode = require('../');
+var strftime = require('strftime');
+
+test('time codes', function (t) {
+    var t0 = new Date(1370000000000); // fri may 31 2013 04:33:20
+    var t1 = new Date(1371000000000); // tue june 11 2013 18:20:00
+    
+    t.equal(decode('\\d'), strftime('%a %b %d'), "now o'clock");
+    t.equal(decode('\\d', { now: t0 }), 'Fri May 31');
+    t.equal(decode('\\d', { now: t1 }), 'Tue Jun 11');
+    t.equal(decode('\\D{%T}', { now: t0 }), '04:33:20');
+    t.equal(decode('\\D{%T <-> %Y}', { now: t1 }), '18:20:00 <-> 2013');
+    t.equal(decode('\\t', { now: t0 }), '04:33:20');
+    t.equal(decode('\\t', { now: t1 }), '18:20:00');
+    t.equal(decode('\\T', { now: t0 }), '04:33:20');
+    t.equal(decode('\\T', { now: t1 }), '06:20:00');
+    t.equal(decode('\\@', { now: t0 }), '04:33:20 AM');
+    t.equal(decode('\\@', { now: t1 }), '06:20:00 PM');
+    t.equal(decode('\\A', { now: t0 }), '04:33');
+    t.equal(decode('\\A', { now: t1 }), '18:20');
+    
+    t.end();
+});
